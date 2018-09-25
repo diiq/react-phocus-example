4eb1f316514bf398910a3da1c8411bc6148f7106
Sam Bleckley <sam@climatum.com>
2018-09-25 10:23:20 -0400

    Fix j/k and delete focus

--- src/todo-item-view/TodoItemView.js
+++ src/todo-item-view/TodoItemView.js
@@ -29,23 +29,33 @@ ActionContextService.addContext("todo-item", {
       name: "Delete",
       defaultKeys: ["Backspace"],
       actOn: item => {
+        const current = document.getElementById(`todo-item-${item.id}`);
+        const previous = current.previousElementSibling;
+        const next = current.nextElementSibling;
+        if (previous) {
+          previous.focus();
+        } else if (next) {
+          next.focus();
+        } else {
+          ActionContextService.triggerAction("add");
+        }
         TodoList.remove(item.id);
       }
     }),
     next: new Action({
       name: "Next",
-      defaultKeys: ["ArrowDown", "k"],
+      defaultKeys: ["ArrowDown", "j"],
       actOn: item => {
-        const current = document.getElementById(`todo-item-${item.id}`)
+        const current = document.getElementById(`todo-item-${item.id}`);
         const next = current.nextElementSibling;
         next && next.focus();
       }
     }),
     previous: new Action({
       name: "previous",
-      defaultKeys: ["ArrowUp", "j"],
+      defaultKeys: ["ArrowUp", "k"],
       actOn: item => {
-        const current = document.getElementById(`todo-item-${item.id}`)
+        const current = document.getElementById(`todo-item-${item.id}`);
         const previous = current.previousElementSibling;
         previous && previous.focus();
       }
@@ -66,11 +76,15 @@ export const TodoItemView = observer(
           className={`todo-item ${item.status}`}
           id={`todo-item-${item.id}`}
         >
-          {item.name}
+          <div className="name">{item.name}</div>
           <div className="buttons">
-            {item.status == "todo" && <PhocusButton action="start"/>}
-            {item.status == "started" && <PhocusButton action="finish" className="success" />}
-            <PhocusButton action="delete" className="warn">&times;</PhocusButton>
+            {item.status === "todo" && <PhocusButton action="start" />}
+            {item.status === "started" && (
+              <PhocusButton action="finish" className="success" />
+            )}
+            <PhocusButton action="delete" className="warn">
+              &times;
+            </PhocusButton>
           </div>
         </PhocusContext>
       );
